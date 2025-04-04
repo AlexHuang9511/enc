f9ab057ddfceff4b7d3a99f7dfe2c1e25870228e
Alex <alexanderw9511@gmail.com>
2025-04-04 12:20:28 -0700

    add user choice

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/_types/_pid_t.h>
 #include <sys/errno.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int encode(char* iter, char* in, char* out, char* path) {
+int encode(char *iter, char *in, char *out, char *path) {
 
   pid_t child = fork();
 
@@ -19,23 +20,46 @@ int encode(char* iter, char* in, char* out, char* path) {
       return errno;
     }
   }
- return 0;
+  return 0;
 }
 
 int main() {
+  char buf[10];
+  char *path = "/usr/bin/openssl";
 
-  printf("bruh\n");
+  printf("1 - encrypt\n");
+  printf("2 - decrypt\n");
+  printf("3 - add\n");
+  printf("-> ");
+  read(STDIN_FILENO, buf, sizeof(buf));
 
-  char* path = "/usr/bin/openssl";
+  printf("input: %s\n", buf);
 
-  int exitCode = encode("1000", "test.txt", "test.enc", path);
+  if (strcmp(buf, "1")) {
 
-  int status;
-  wait(&status);
+    printf("one\n");
+    int exitCode = encode("1000", "test.txt", "test.enc", path);
+    int status;
+    wait(&status);
+    if (exitCode != 0) {
+      printf("exitCode: %i\n", exitCode);
+    }
 
-  printf("errno: %i\n", exitCode);
+  } else if (strcmp(buf, "2")) {
+
+    printf("two\n");
+
+  } else if (strcmp(buf, "3")) {
+
+    printf("three\n");
+
+  } else {
+    printf("unknown: '%s'\n", buf);
+  }
 
 
+
+  //printf("errno: %i\n", exitCode);
 
   return 0;
 }
